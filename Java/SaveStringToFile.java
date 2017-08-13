822f03a1ddf667b684bef5735062d587c288d6b3
Qian Yilun <qianyilun@gmail.com>
2017-08-12 20:13:01 -0700

    saveStringToFile.java

---
+++ Java/SaveStringToFile.java
@@ -0,0 +1,21 @@
+// https://stackoverflow.com/questions/1053467/how-do-i-save-a-string-to-a-text-file-using-java
+void saveToFile(String formattedJson) {
+    BufferedWriter writer = null;
+    try {
+        writer = new BufferedWriter( new FileWriter("history.json"));
+        writer.write(formattedJson);
+
+    }
+    catch (IOException e) {
+        e.printStackTrace();
+    }
+    finally {
+        try {
+            if (writer != null) {
+                writer.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
