3b7c2ed089b7bed943a7abb8777321607f7512f9
qianyilun <qianyilun@gmail.com>
2017-08-13 21:18:51 -0700

    (1)add IRT.java (2)be able to launch every QM by 5 sec (3)implement sort method of IRT

--- Java/PrintCurrentTimestamp.java
+++ Java/PrintCurrentTimestamp.java
@@ -1,3 +1,4 @@
+//https://www.mkyong.com/java/java-how-to-get-current-date-time-date-and-calender/
 public void printTimestamp() {
 	DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 	Date date = new Date();
