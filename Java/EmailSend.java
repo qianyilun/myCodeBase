c6611caaec4896b3ddf8230131cd105f1ae4b5e6
Yilun Qian <qianyilun@gmail.com>
2017-08-22 21:34:20 -0700

    update the EmailSend.java comments

--- Java/EmailSend.java
+++ Java/EmailSend.java
@@ -1,10 +1,8 @@
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
- * https://www.google.com/settings/security/lesssecureapps
+ * Note, need to modify via https://www.google.com/settings/security/lesssecureapps
  */
-
-package SendEmail;
 
 /**
  *
